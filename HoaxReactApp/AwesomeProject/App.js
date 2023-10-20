088b1b8a7ad0c95fd7c8de1e951fcee26f90d4d1
pppbigbud <pppbigbud@gmail.com>
2023-10-20 10:12:46 +0200

    informationJsonStorageOK

--- HoaxReactApp/AwesomeProject/App.js
+++ HoaxReactApp/AwesomeProject/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 import Navigation from './src/Navigation.js';
 
 export default function App() {
